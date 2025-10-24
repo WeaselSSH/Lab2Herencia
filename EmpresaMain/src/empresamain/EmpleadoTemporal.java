040e930ad57d550e1c5137a30b665e3eb60ef50b
EstebanFerS <estebanfernandez865@gmail.com>
2025-10-24 09:08:58 -0600

    Clase Empleado Temporal

---
+++ EmpresaMain/src/empresamain/EmpleadoTemporal.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package empresamain;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author esteb
+ */
+public class EmpleadoTemporal extends Empleado{
+    private Calendar fechaFin;
+    
+    public EmpleadoTemporal(int codigoEmp, String nombreEmp, double salarioBase, Calendar fechaFin){
+        super(codigoEmp, nombreEmp, salarioBase);
+        this.fechaFin = fechaFin;
+    }
+    
+    public double calcularPago(){
+        Calendar hoy = Calendar.getInstance();
+        if(hoy.compareTo(fechaFin) <= 0)
+            return super.calcularPago();
+        return 0;
+    }
+    
+    public void actualizarFecha(Calendar fechaNueva){
+        this.fechaFin = fechaNueva;
+    }
+    
+    public String mostrarInfo(){
+        SimpleDateFormat fechaFormateada = new SimpleDateFormat("dd/MM/yyyy");
+        return super.mostrarInfo() + 
+                "\nFecha Fin del contrato: " + fechaFormateada.format(fechaFin.getTime());
+    }
+}
